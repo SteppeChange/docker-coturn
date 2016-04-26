c7f3af3c0e3a08fb756f8364d15bd759b3b24d33
Ubuntu <ubuntu@ip-172-30-1-191.ec2.internal>
2016-04-26 12:01:29 +0000

    BUTTON-1477 remove unsecure log

--- turn.sh
+++ turn.sh
@@ -3,6 +3,7 @@ cd ~
 
 rm -f ./external_ip
 rm -f ./etcd_ip
+rm -f ./etcd_val
 
 if [ -z "$ETCD_ADDRESS" ]; then
     curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null > ./etcd_ip
@@ -23,14 +24,21 @@ export ETCD_ADDRESS=`cat ./etcd_ip`
 echo external ip $EXTERNAL_IP
 echo etcd url $ETCD_ADDRESS
 
+#export ETCD_ADDRESS=172.30.1.152:2379
+
 curl -L "http://$ETCD_ADDRESS/v2/keys/config/bws/server/call_secret" | grep -Po '"value":.*?[^\\]",' > etcd_val
 export ETCD_VAL=`cat ./etcd_val`
 echo ${ETCD_VAL:9:-2} > etcd_key
 export SECRET_KEY=`cat ./etcd_key`
 
-echo key $SECRET_KEY
+echo key $SECRET_KEY | sed 's/[0-9]*/X/g'
+
+if [ -z "$SECRET_KEY" ]; then
+  echo "ERROR: \$SECRET_KEY is wrong" 
+  exit 1
+fi
 
 # service syslog-ng start
 
-/usr/local/bin/turnserver -c /opt/coturn/etc/turnserver.conf
+#/usr/local/bin/turnserver -c /opt/coturn/etc/turnserver.conf
 
